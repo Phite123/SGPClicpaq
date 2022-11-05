e1122644e384e920e09ca5c746978acb1bba5d40
Pedro <Pedro@DESKTOP-IITBQKP>
2022-11-05 13:09:37 -0300

    Agregar archivos de proyecto.

---
+++ SGPClicpaq/Server/Context/IANContext.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using SGPClicpaq.Shared.Models;
+
+namespace SGPClicpaq.Server.Context
+{
+    public partial class IANContext : DbContext
+    {
+        public IANContext()
+        {
+        }
+
+        public IANContext(DbContextOptions<IANContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Viaje> Viajes { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("server=192.168.0.12;user=consulta;password=consulta;database=IAN");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Viaje>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Chofer)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("chofer");
+
+                entity.Property(e => e.Estado).HasColumnName("estado");
+
+                entity.Property(e => e.EstadoStr)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("estadoStr");
+
+                entity.Property(e => e.FechaViaje)
+                    .HasColumnType("date")
+                    .HasColumnName("fechaViaje");
+
+                entity.Property(e => e.Fin)
+                    .HasColumnType("datetime")
+                    .HasColumnName("fin");
+
+                entity.Property(e => e.Guias)
+                    .IsUnicode(false)
+                    .HasColumnName("guias");
+
+                entity.Property(e => e.Inicio)
+                    .HasColumnType("datetime")
+                    .HasColumnName("inicio");
+
+                entity.Property(e => e.Kilometros).HasColumnName("kilometros");
+
+                entity.Property(e => e.Observaciones)
+                    .IsUnicode(false)
+                    .HasColumnName("observaciones");
+
+                entity.Property(e => e.Sucursal).HasColumnName("sucursal");
+
+                entity.Property(e => e.TimeCargando)
+                    .HasColumnType("datetime")
+                    .HasColumnName("timeCargando");
+
+                entity.Property(e => e.TimeEnCamion)
+                    .HasColumnType("datetime")
+                    .HasColumnName("timeEnCamion");
+
+                entity.Property(e => e.TimeGenerado)
+                    .HasColumnType("datetime")
+                    .HasColumnName("timeGenerado");
+
+                entity.Property(e => e.TimeSaleCamion)
+                    .HasColumnType("datetime")
+                    .HasColumnName("timeSaleCamion");
+
+                entity.Property(e => e.TotalBultos).HasColumnName("totalBultos");
+
+                entity.Property(e => e.TotalGuias).HasColumnName("totalGuias");
+
+                entity.Property(e => e.TotalPeso).HasColumnName("totalPeso");
+
+                entity.Property(e => e.TotalVolumen).HasColumnName("totalVolumen");
+
+                entity.Property(e => e.Transporte)
+                    .IsUnicode(false)
+                    .HasColumnName("transporte");
+
+                entity.Property(e => e.Unidad)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("unidad");
+
+                entity.Property(e => e.ZonaDestino)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("zonaDestino");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ SGPClicpaq/Shared/Models/Viaje.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SGPClicpaq.Shared.Models
+{
+    public partial class Viaje
+    {
+        public int Id { get; set; }
+        public string? Guias { get; set; }
+        public string? ZonaDestino { get; set; }
+        public DateTime? Inicio { get; set; }
+        public DateTime? Fin { get; set; }
+        public string? Observaciones { get; set; }
+        public DateTime? FechaViaje { get; set; }
+        public string? Unidad { get; set; }
+        public string? Chofer { get; set; }
+        public int? Estado { get; set; }
+        public DateTime? TimeGenerado { get; set; }
+        public DateTime? TimeCargando { get; set; }
+        public DateTime? TimeEnCamion { get; set; }
+        public DateTime? TimeSaleCamion { get; set; }
+        public int? Sucursal { get; set; }
+        public int? TotalGuias { get; set; }
+        public int? TotalBultos { get; set; }
+        public int? TotalVolumen { get; set; }
+        public int? TotalPeso { get; set; }
+        public string? Transporte { get; set; }
+        public string? EstadoStr { get; set; }
+        public int? Kilometros { get; set; }
+    }
+}
